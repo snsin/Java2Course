4c73b2182e5eea93fcf327d1303573872b78429e
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-01-31 22:08:08 +0300

    Befor finManager

--- Java2/src/ru/geekbrains/hw1/Main.java
+++ Java2/src/ru/geekbrains/hw1/Main.java
@@ -1,23 +1,31 @@
 package ru.geekbrains.hw1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		GenericStack<String> ststr = new GenericStack<>();
 		try {
 			ststr.push("ОГО");
-			ststr.push("Star");
-			ststr.push("one");
-			ststr.push("two");
 			ststr.push(new Integer(983240189).toString());
-			ststr.push(new Integer(99));
 			while (! ststr.isEmpty()) {
 				System.out.println(ststr.pop());
 			}
 		} catch (StackException e) {
 			e.printStackTrace();
 		}
-
+		List<String> ls = Arrays.asList("aaa", "bb", "c");
+		System.out.println(ls);
+		Collections.sort(ls, new LengthComparator());
+		System.out.println(ls);
+		System.out.println(EnuTest.THIS);
+		System.out.println(Double.compare(Double.MAX_VALUE, Double.POSITIVE_INFINITY));
+		Double.compare(0.0, Double.NaN);
+		
 	}
+	
 
 }
