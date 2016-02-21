bfc9fdf34d8b69c1e373241eceb4bd182c4d691a
smac <sin_s_n@mail.ru>
2016-02-22 01:48:14 +0300

    method for create tables in DbHelper

--- Java2/src/ru/geekbrains/finmanager/DbHelper.java
+++ Java2/src/ru/geekbrains/finmanager/DbHelper.java
@@ -38,13 +38,41 @@ public class DbHelper {
 		
 	}
 
-	private static void createTables() {
+	private static void createTables() throws SQLException {
 		
-		PreparedStatement stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS "public"."users" (
-	id SERIAL PRIMARY KEY,
-	"login" TEXT NOT NULL UNIQUE,
-	"pass" TEXT NOT NULL
-);");		
+		PreparedStatement stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS public.users ("
+				+ "id SERIAL PRIMARY KEY,"
+				+ "	login TEXT NOT NULL UNIQUE,"
+				+ "	pass TEXT NOT NULL);");	
+		stm.executeUpdate();
+		
+		stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS public.accounts ("
+				+ "	id SERIAL PRIMARY KEY,"
+				+ "	user_id integer REFERENCES public.users,"
+				+ "	balance numeric,"
+				+ "	description text);");
+		stm.executeUpdate();
+		stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS public.records ("
+				+ "	id SERIAL PRIMARY KEY,"
+				+ "	account_id integer REFERENCES public.accounts,"
+				+ "	transfer integer CHECK(transfer = -1 OR transfer = 1),"
+				+ "	date timestamp,"
+				+ "	amount numeric,"
+				+ "	description text);");
+		stm.executeUpdate();
+		
+		stm = conn.prepareStatement("create table if not exists public.categories_names"
+				+ "	id SERIAL PRIMARY KEY,"
+				+ "	name text NOT NULL UNIQUE);");
+		stm.executeUpdate();
+		
+		stm = conn.prepareStatement("CREATE TABLE IF NOT EXISTS public.categories ("
+				+ "	id SERIAL PRIMARY KEY,"
+				+ "	record_id integer references public.records,"
+				+ "	name text references public.categories_names (name),"
+				+ "	description text);");
+		stm.executeUpdate();
+		
 	}
 
 }
