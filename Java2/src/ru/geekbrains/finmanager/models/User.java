496d706e93b90cdc70f9e427c247fed808a57210
smac <sin_s_n@mail.ru>
2016-02-22 17:01:59 +0300

    Removed garbage

--- Java2/src/ru/geekbrains/finmanager/models/User.java
+++ Java2/src/ru/geekbrains/finmanager/models/User.java
@@ -2,12 +2,6 @@ package ru.geekbrains.finmanager.models;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 
 public class User {
 	private int userId = 0;
