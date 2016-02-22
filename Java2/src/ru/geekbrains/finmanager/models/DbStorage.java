ac72895aa4fd14d0fcb31edee00069809c16a328
smac <sin_s_n@mail.ru>
2016-02-22 17:03:06 +0300

    Removed garbage

--- Java2/src/ru/geekbrains/finmanager/models/DbStorage.java
+++ Java2/src/ru/geekbrains/finmanager/models/DbStorage.java
@@ -1,13 +1,10 @@
 package ru.geekbrains.finmanager.models;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import ru.geekbrains.finmanager.DbHelper;
