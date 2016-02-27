721c386c035f504e540cbb80a6899ec371bd3d9f
smac <sin_s_n@mail.ru>
2016-02-27 13:57:17 +0300

    Garbage fields and methods are removed from User

--- Java2/src/ru/geekbrains/finmanager/models/User.java
+++ Java2/src/ru/geekbrains/finmanager/models/User.java
@@ -1,13 +1,12 @@
 package ru.geekbrains.finmanager.models;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 public class User {
 	private int userId = 0;
 	private String name;
 	private String passHash;
-	private boolean loggedIn = false;
+
 
 
 	public User(String name, String passHash) {
@@ -22,26 +21,11 @@ public class User {
 		this.passHash = passHash;
 	};
 
-	public static String password(String pass) {
-		MessageDigest md5sum = null;
-		try {
-			md5sum = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			System.exit(SysErrs.PASS_GENERATE_ERR.ordinal());
-		}
-		byte[] rawSum = md5sum.digest(pass.getBytes());
-		StringBuilder passHash = new StringBuilder();
-		for (byte b : rawSum) {
-			passHash.append(String.format("%02x", b));
-		}
-		return passHash.toString();
-	}
 
-	public boolean login(String pass) {
+/*	public boolean login(String pass) {
 		this.loggedIn = this.passHash.equals(password(pass));
 		return this.loggedIn;
-	}
+	}*/
 
 	public String getName() {
 		return name;
