950a9fff907313beab32c94a17a55a5703fdedf3
smac <sin_s_n@mail.ru>
2016-02-27 13:55:37 +0300

    PassMD5 is realized

--- Java2/src/ru/geekbrains/finmanager/controllers/PassMD5.java
+++ Java2/src/ru/geekbrains/finmanager/controllers/PassMD5.java
@@ -1,11 +1,28 @@
 package ru.geekbrains.finmanager.controllers;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import ru.geekbrains.finmanager.models.SysErrs;
+
 public class PassMD5 implements Password {
 
 	@Override
 	public String passHash(String password) {
-		// TODO Auto-generated method stub
-		return null;
+		MessageDigest md5sum = null;
+		try {
+			md5sum = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			System.exit(SysErrs.PASS_GENERATE_ERR.ordinal());
+		}
+		byte[] rawSum = md5sum.digest(password.getBytes());
+		StringBuilder passHash = new StringBuilder();
+		for (byte b : rawSum) {
+			passHash.append(String.format("%02x", b));
+		}
+		return passHash.toString();
+
 	}
 
 }
--- Java2/src/ru/geekbrains/finmanager/controllers/Password.java
+++ Java2/src/ru/geekbrains/finmanager/controllers/Password.java
@@ -2,5 +2,4 @@ package ru.geekbrains.finmanager.controllers;
 
 public interface Password {
 	String passHash(String password);
-
 }
