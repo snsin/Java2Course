8dc257e4d78d1497667e7f858531e8da1dbd4c26
smac <sin_s_n@mail.ru>
2016-02-27 15:04:48 +0300

    SignIn is finished

--- Java2/src/ru/geekbrains/finmanager/controllers/SignIn.java
+++ Java2/src/ru/geekbrains/finmanager/controllers/SignIn.java
@@ -1,5 +1,29 @@
 package ru.geekbrains.finmanager.controllers;
 
+import ru.geekbrains.finmanager.models.DataStore;
+import ru.geekbrains.finmanager.models.User;
+
 public class SignIn {
+	private Password password;
+	private DataStore storage;
+	
+	public SignIn(Password password, DataStore storage) {
+		this.password = password;
+		this.storage = storage;
+	}
+	
+	public boolean isNameExist(String name) {
+		return storage.getUserNames().contains(name);
+	}
+	
+	public User createUser(String name, String password) {
+		User result = null;
+		if (!isNameExist(name)) {
+			String passHash = this.password.passHash(password);
+			storage.addUser(new User(name, passHash));
+			result = storage.getUser(name);
+		}
+		return result;	
+	}
 
 }
