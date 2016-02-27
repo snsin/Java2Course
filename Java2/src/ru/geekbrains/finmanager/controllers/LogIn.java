053244e9fc992592c6e9dcca07f614446027c836
smac <sin_s_n@mail.ru>
2016-02-27 13:45:38 +0300

    LogIn is finished

--- Java2/src/ru/geekbrains/finmanager/controllers/LogIn.java
+++ Java2/src/ru/geekbrains/finmanager/controllers/LogIn.java
@@ -1,5 +1,24 @@
 package ru.geekbrains.finmanager.controllers;
 
+import ru.geekbrains.finmanager.models.DataStore;
+import ru.geekbrains.finmanager.models.User;
+
 public class LogIn {
+	private Password password;
+	private DataStore storage;
+	
+	public LogIn(Password password, DataStore store) {
+		this.password = password;
+		this.storage = store;
+	}
+	
+	public User login(String userName, String password) {
+		User result = storage.getUser(userName);
+		if (result != null) {
+			String currentHash = this.password.passHash(password);
+			result = currentHash.equals(result.getPassHash()) ? result : null;
+		}
+		return result;
+	}
 
 }
