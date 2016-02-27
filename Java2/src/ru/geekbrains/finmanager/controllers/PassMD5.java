13fa334f4e249e6fd0ef000ecbef3ca553ed2c1f
smac <sin_s_n@mail.ru>
2016-02-27 13:48:54 +0300

    PassMD5 is added

---
+++ Java2/src/ru/geekbrains/finmanager/controllers/PassMD5.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.finmanager.controllers;
+
+public class PassMD5 implements Password {
+
+	@Override
+	public String passHash(String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
