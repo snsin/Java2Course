f2a84318cf1bc7ffd1992ecc92367bc1890068d8
smac <sin_s_n@mail.ru>
2016-02-28 19:06:28 +0300

    CharHistogram added

---
+++ Java2/hw3/ru/geekbrains/algorithms/CharHistogram.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharHistogram {
+	
+	public static Map<String, Integer> hist(String source) {
+		Map<String, Integer> result = null;
+		if (source != null) {
+			result = new HashMap<>();
+			
+		}
+		return result;
+	}
+
+}
