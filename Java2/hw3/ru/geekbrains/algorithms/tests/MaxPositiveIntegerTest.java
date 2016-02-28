e5809cc4b38d2359d710a4835da484433976b1db
smac <sin_s_n@mail.ru>
2016-02-28 15:53:03 +0300

    Test realized

--- Java2/hw3/ru/geekbrains/algorithms/tests/MaxPositiveIntegerTest.java
+++ Java2/hw3/ru/geekbrains/algorithms/tests/MaxPositiveIntegerTest.java
@@ -2,13 +2,30 @@ package ru.geekbrains.algorithms.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import ru.geekbrains.algorithms.MaxPositiveInteger;
+
 public class MaxPositiveIntegerTest {
+	private int[] testArray;
+	
+	@Before
+	public void createArray() {
+		testArray = new int[15];
+		for (int i = 1; i < testArray.length; i++) {
+			testArray[i] = i + 10;
+		}
+		testArray[0] = 0;
+
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(testArray.length + 9, MaxPositiveInteger.max(testArray));
+
 	}
 
 }
