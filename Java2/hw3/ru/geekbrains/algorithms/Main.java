cd488d1cb741f7ce9d8b08a68a991fa04fb329b4
smac <sin_s_n@mail.ru>
2016-02-28 19:44:25 +0300

    cH

--- Java2/hw3/ru/geekbrains/algorithms/Main.java
+++ Java2/hw3/ru/geekbrains/algorithms/Main.java
@@ -1,9 +1,19 @@
 package ru.geekbrains.algorithms;
 
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String str = "kjdsla;jn vjkpanejuipnvdajnfcxzlvjnbiwapeufusdfjkl"
+				+ "asd;jfkljasl;fj;ldjsakfl;jl;fjknvkjcx;nvj;nhewuroqngjvcb"
+				+ "ldsfajl;jkasljfkldajskfDSFAASDFVCVEWHNKJ,,JJHGKMBl;asjdfoi"
+				+ "dfsadsaffdsafdsafSA,IUOPHJKdsafsdafdsafvcgfrbfgnhhgdsfgdfv";
 		System.out.println(ReverseString.reverse("safjk;sadjf"));
+		Map<String, Integer> hist = CharHistogram.hist(str);
+		for (String ch : hist.keySet()){
+			System.out.println(ch +": " + hist.get(ch));
+		}
 
 	}
 
