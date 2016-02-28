72199ba5166eb715e199f7bfb0d9cd3a705c5daa
smac <sin_s_n@mail.ru>
2016-02-28 15:37:07 +0300

    MaxPositiveIntegerTest created

---
+++ Java2/hw3/ru/geekbrains/algorithms/tests/MaxPositiveIntegerTest.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.algorithms.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaxPositiveIntegerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
