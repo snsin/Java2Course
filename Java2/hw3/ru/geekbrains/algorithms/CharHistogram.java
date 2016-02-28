7d90a4ccb525a1c69c0432d1252bf281525a0997
smac <sin_s_n@mail.ru>
2016-02-28 19:20:36 +0300

    CharHistogram finished

--- Java2/hw3/ru/geekbrains/algorithms/CharHistogram.java
+++ Java2/hw3/ru/geekbrains/algorithms/CharHistogram.java
@@ -9,7 +9,13 @@ public class CharHistogram {
 		Map<String, Integer> result = null;
 		if (source != null) {
 			result = new HashMap<>();
-			
+			char[] chars = source.toCharArray();
+			for (char c : chars) {
+				Integer count = result.put(String.valueOf(c), 1);
+				if (count != null) {
+					result.put(String.valueOf(c), count + 1);
+				}
+			}
 		}
 		return result;
 	}
