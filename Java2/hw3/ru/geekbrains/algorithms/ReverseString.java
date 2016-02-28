8035e25ab325dabd9cfc955282ca7f7e38a38935
smac <sin_s_n@mail.ru>
2016-02-28 17:54:12 +0300

    ReverseString added

---
+++ Java2/hw3/ru/geekbrains/algorithms/ReverseString.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.algorithms;
+
+public class ReverseString {
+	public static String reverse(String suorce) {
+		StringBuilder result = new StringBuilder();
+		return result.toString();
+	}
+}
