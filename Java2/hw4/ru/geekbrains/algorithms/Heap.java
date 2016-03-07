53df5cbc7fd27deddfb8d651bf2450de5b8109a3
smac <sin_s_n@mail.ru>
2016-03-07 22:42:40 +0300

    changes

--- Java2/hw4/ru/geekbrains/algorithms/Heap.java
+++ Java2/hw4/ru/geekbrains/algorithms/Heap.java
@@ -17,26 +17,10 @@ public class Heap<T extends Comparable<T>> {
 		heap = (T[]) new Object[size];
 	}
 	
-	private int parent(int i) {
-		return (i - 1) / 2;
-	}
-
-	private int left(int i) {
-		return 2 * i + 1;
-	}
-
-	private int rigth(int i) {
-		return 2 * i + 2;
-	}
-	
 	public int getSize() {
 		return heapSize;
 	}
-	
-	private boolean isCorrectIndex(int i) {
-		return ((i >= 0) && (i < heapSize));
-	}
-	
+
 	void maxHeapify (int i) {
 		int left = left(i);
 		int right = rigth(i);
@@ -52,7 +36,7 @@ public class Heap<T extends Comparable<T>> {
 			maxHeapify(largest);
 		}
 	}
-	
+
 	T extractMax() {
 		T result = null;
 		if (heapSize > 0) {
@@ -62,7 +46,7 @@ public class Heap<T extends Comparable<T>> {
 		}
 		return result;
 	}
-	
+
 	void insert(T elem){
 		incrementSize();
 		heap[heapSize - 1] = heap[0];
@@ -70,8 +54,26 @@ public class Heap<T extends Comparable<T>> {
 		maxHeapify(0);
 	}
 
+	T getMax() {
+		return heap[0];
+	}
+	
+	private int parent(int i) {
+		return (i - 1) / 2;
+	}
 
+	private int left(int i) {
+		return 2 * i + 1;
+	}
 
+	private int rigth(int i) {
+		return 2 * i + 2;
+	}
+	
+	private boolean isCorrectIndex(int i) {
+		return ((i >= 0) && (i < heapSize));
+	}
+	
 	private void swap(int i, int k) {
 		if (i != k) {
 			T temp = heap[i];
