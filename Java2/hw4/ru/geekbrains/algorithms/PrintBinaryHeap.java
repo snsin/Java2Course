ed224f929dd397099ddd473c4677925712692911
smac <sin_s_n@mail.ru>
2016-03-07 23:38:47 +0300

    trash

--- Java2/hw4/ru/geekbrains/algorithms/PrintBinaryHeap.java
+++ Java2/hw4/ru/geekbrains/algorithms/PrintBinaryHeap.java
@@ -1,14 +1,24 @@
 package ru.geekbrains.algorithms;
 
-public class PrintBinaryHeap<T extends Heap> {
+public class PrintBinaryHeap {
 	int firstIndent;
 	
-	public PrintBinaryHeap(T heap) {
+	public <T extends Heap> PrintBinaryHeap(T heap) {
 		int heigth = (int) (Math.log(heap.getSize())/Math.log(Math.E));
 		firstIndent = heigth * 3 + heap.getMax().toString().length();
 	}
-	public void print(T heap) {
+	public <T> void printElem(T heapElem) {
+		System.out.println(heapElem);
+		System.out.println();
+		System.out.printf("%s", "a");
+	}
+	
+	private int left(int i) {
+		return 2 * i + 1;
+	}
 
+	private int rigth(int i) {
+		return 2 * i + 2;
 	}
 
 }
