d77ce23e788f69342620e287e44f61f2a4ab071b
smac <sin_s_n@mail.ru>
2016-03-08 12:45:23 +0300

    added HeapQueue

---
+++ Java2/hw4/ru/geekbrains/algorithms/HeapQueue.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.algorithms;
+
+public class HeapQueue<K extends Comparable<K>, T> implements PriorityQueue<K, T> {
+
+	@Override
+	public T getMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(K k, T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
