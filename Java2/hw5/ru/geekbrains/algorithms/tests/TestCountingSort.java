193367e9fba0a1d102cb95a6806ce9d0f2e9bfb2
smac <sin_s_n@mail.ru>
2016-03-13 20:39:03 +0300

    tests realized

--- Java2/hw5/ru/geekbrains/algorithms/tests/TestCountingSort.java
+++ Java2/hw5/ru/geekbrains/algorithms/tests/TestCountingSort.java
@@ -2,18 +2,75 @@ package ru.geekbrains.algorithms.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestCountingSort {
+import ru.geekbrains.algorithms.CountingSort;
 
+public class TestCountingSort {
+	Random rnd;
+	int[] bestCaseArr;
+	int[] stupidArr;
+	int[] zeroLengthArr;
+	int[] negativeElementArray;
+	
 	@Before
 	public void setUp() throws Exception {
+		rnd = new Random();
+		int max = 100;
+		bestCaseArr = new int[1000*max];
+		for (int i = 0; i < bestCaseArr.length; i++) {
+			bestCaseArr[i] = rnd.nextInt(max);
+		}
+		negativeElementArray = new int[bestCaseArr.length];
+		System.arraycopy(bestCaseArr, 0, negativeElementArray, 0, bestCaseArr.length);
+		negativeElementArray[bestCaseArr.length / 2] = -1;
+		stupidArr = new int[] {0, 100 * max};
+		zeroLengthArr = new int[0];
 	}
 
 	@Test
-	public void testSort() {
-		fail("Not yet implemented");
+	public void testBestCase() {	
+		assertTrue(isSorted(CountingSort.sort(bestCaseArr)));
+	}
+	
+	@Test
+	public void testStupidButShouldWork() {
+		assertTrue(isSorted(CountingSort.sort(stupidArr)));
+	}
+	
+	@Test
+	public void testNullPointer() {
+		assertNull(CountingSort.sort(null));
+	}
+	
+	@Test
+	public void testZeroLength() {
+		assertNull(CountingSort.sort(zeroLengthArr));
+	}
+	
+	@Test
+	public void testNegativeElement() {
+		assertNull(CountingSort.sort(negativeElementArray));
+	}
+	
+	private boolean isSorted(int[] array) {
+		boolean result =false;
+		if (array != null && array.length > 0) {
+			int previos = array[0];
+			result = true;
+			for (int e : array) {
+				if (e < previos) {
+					result = false;
+					break;					
+				} else {
+					previos = e;
+				}
+			}
+		}
+		return result;
 	}
 
 }
